e8a4be5c6ab9bf6fc263f7fb66137e98ca5bc1c6
Antony Rinaldi <antony@atyrnal.dev>
2025-09-14 14:38:15 -0400

    Add Slint GUI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 use std::collections::HashMap;
 use std::env;
-use std::fs::{self, File};
-use std::io::{self, BufRead, BufReader};
+use std::fs::{File};
+use std::io::{BufRead, BufReader};
+use std::rc::Rc;
+use std::cell::RefCell;
+use chrono::Local;
+use uuid::Uuid;
+
+slint::include_modules!();
 
 const TARGETED_PROPERTIES : [&str; 6] = ["total filament used [g]", "filament_type", "printer_model", "printer_settings_id", "printer_notes", "estimated printing time (normal mode)"];
+
+#[allow(non_snake_case)]
+#[derive(Debug)]
+#[allow(dead_code)] //Remove this eventually
+struct Print {
+    HumanName : String,
+    Email : String,
+    PrinterName : String,
+    FileName : String,
+    FilamentType : String,
+    FilamentOwner : String,
+    FilamentWeight : String,
+    Time : String,
+    UUID : String,
+    Timestamp : i64
+}
 
 fn main() {
     //Get args / gcode file path from OrcaSlicer
@@ -31,9 +53,75 @@ fn main() {
         }
     }
 
-    println!("{:#?}", gcode_properties);
+    #[allow(unused_variables)] //Remove eventually
+    let print: Print;
+
+    match gui() {
+        Ok((human_name, email, filament_owner)) => {
+            print = Print {
+                HumanName : human_name,
+                Email : email,
+                PrinterName : {
+                    let full_string : String = gcode_properties[TARGETED_PROPERTIES[3]].clone();
+                    let printer_type: String = full_string.split("(").nth(0).unwrap_or("").trim().to_string();
+                    let printer_number: String = full_string.split("#").nth(1).unwrap_or("").split(" ").nth(0).unwrap().trim().to_string();
+                    let printer_name: String = full_string.split("#").nth(1).unwrap_or("").split_whitespace().skip(1).collect::<Vec<&str>>().join(" ").trim().to_string();
+
+                    format!("{} #{} - {}", printer_type, printer_number, printer_name)
+                },
+                FileName : {
+                    let filepath: String = gcode_path.clone();
+                    let fp_split = filepath.split("/").last().unwrap_or("").trim().split(".");
+                    fp_split.clone().take(fp_split.count()-1).collect::<Vec<&str>>().join(".").trim().to_string()
+                },
+                FilamentType : gcode_properties[TARGETED_PROPERTIES[1]].clone(),
+                FilamentOwner : filament_owner,
+                FilamentWeight : format!("{}g", gcode_properties[TARGETED_PROPERTIES[0]]),
+                Time : Local::now().format("%b %e, %Y, %I:%M:%S %p").to_string(),
+                UUID : Uuid::new_v4().to_string(),
+                Timestamp : Local::now().timestamp()
+            };
+            println!("Dialog Submitted")
+        }
+        Err(e) => {
+            eprintln!("GUI error: {}", e);
+            std::process::exit(1);
+        }
+    }
+
+    println!("{:#?}", print);
+
     std::io::stdin().read_line(&mut String::new()).unwrap();
 
+}
+
+fn gui() -> Result<(String, String, String), slint::PlatformError> {
+    let app = AppWindow::new().unwrap();
+
+    let result : Rc<RefCell<Option<(String, String, String)>>> = Rc::new(RefCell::new(None)); 
+
+    app.on_sumbit({
+        let app_weak = app.as_weak();
+        let result_clone = result.clone();
+        move |human_name : slint::SharedString, email : slint::SharedString, filament_owner : slint::SharedString| {
+            let app = app_weak.unwrap();
+            *result_clone.borrow_mut() = Some((
+                human_name.to_string(), 
+                email.to_string(), 
+                filament_owner.to_string()
+            ));
+            let _ = app.hide();
+        }
+    });
+
+    app.run()?;
+
+    match result.borrow().as_ref() {
+        Some(data) => Ok(data.clone()),
+        None => {
+            Err(slint::PlatformError::Other("No data submitted".into()))
+        }
+    }
 }
 
 //Label information:
